788fce0d8874ffc3d64f05d0bdb71d1882016ca4
Kzsombor5 <kabai.zsombi@gmail.com>
2025-09-25 11:48:33 +0200

    dasdas

--- REACT-NAVBAR/src/App.jsx
+++ REACT-NAVBAR/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Contact from "./pages/Contact"
+import Contact from "./pages/Contact" 
 import Services from "./pages/Services"
 
 function App() {
